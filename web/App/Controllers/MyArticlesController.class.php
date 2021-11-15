8be6bf0490b8f62848e0a0aedea7143478e98aec
mira <mira.vdov@gmail.com>
2021-11-16 00:20:31 +0100

    zprovozneni pdf dokumentu

--- web/App/Controllers/MyArticlesController.class.php
+++ web/App/Controllers/MyArticlesController.class.php
@@ -36,10 +36,26 @@ class MyArticlesController implements IController
         $tplData["user"] = $this->um->getUserInfo();
         $tplData["userRight"] = $this->um->getUserRightInfo();
         // pokud mám uživatele tak si uložím všechny jeho články
-        if ($tplData["user"] != null)$tplData["userArticles"] = $this->articlesManager->getAllUsersArticles($tplData["user"]["id_uzivatel"]);
+        if ($tplData["user"] != null){
+
+            // vytvořenínového článku
+            if (isset($_POST["action"]) && $_POST["action"] == "createNewArticle"){
+                if ($_POST["title"] != ""){
+                    if (!empty($_FILES["pdf_file"]["name"]) && $_FILES["pdf_file"]["error"] == 0){
+                        $this->articlesManager->createNewArticle($_POST["title"], "asdsad", $tplData["user"]["id_uzivatel"]);
+                    }
+                }
+            }
+
+            $tplData["userArticles"] = $this->articlesManager->getAllUsersArticles($tplData["user"]["id_uzivatel"]);
+        }
+
+        // rozkodování clanku
+        for ($i = 0; $i < sizeof($tplData["userArticles"]); $i++){
+            $tplData["userArticles"][$i]["pdf"] = base64_encode($tplData["userArticles"][$i]["pdf"]);
+        }
 
         Helper::loginHelp($this->um);
-
         return $tplData;
     }
 }
--- web/App/Models/ArticlesManagerModel.class.php
+++ web/App/Models/ArticlesManagerModel.class.php
@@ -3,6 +3,8 @@
 namespace app\Models;
 
 use app\Models\DatabaseManagerModel as DM;
+use Cassandra\Date;
+use mysql_xdevapi\Exception;
 
 /**
  * Třída sloužící ke správě článků
@@ -32,5 +34,16 @@ class ArticlesManagerModel
     public function getAllUsersArticles(int $userID){
 
         return $this->databaseManager->selectFromTable(TABLE_ARTICLES, "id_uzivatel=$userID", "datum DESC");
+    }
+
+    public function createNewArticle(string $name, string $content, int $userID){
+        $date = date('Y-m-d H:i:s');
+
+        $file_tmp = $_FILES['pdf_file']['tmp_name']; // cesta k souboru
+        $file = addslashes(file_get_contents($file_tmp));
+
+        $insertStatement = "obsah, datum, id_uzivatel, nadpis, pdf";
+        $insertValues = "'$content', '$date','$userID', '$name', '$file'";
+        $this->databaseManager->insertIntoTable(TABLE_ARTICLES, $insertStatement, $insertValues);
     }
 }
