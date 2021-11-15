4d613d8f4df44172189e1a9b55cccb9bc85cad2b
mira <mira.vdov@gmail.com>
2021-11-15 17:07:58 +0100

    pridani pridan clanku

--- web/App/Models/ArticlesManagerModel.class.php
+++ web/App/Models/ArticlesManagerModel.class.php
@@ -4,6 +4,9 @@ namespace app\Models;
 
 use app\Models\DatabaseManagerModel as DM;
 
+/**
+ * Třída sloužící ke správě článků
+ */
 class ArticlesManagerModel
 {
     private $databaseManager;
@@ -13,13 +16,21 @@ class ArticlesManagerModel
         $this->databaseManager = DM::getDatabaseModel();
     }
 
-
+    /**
+     * Metoda vrací všechny články
+     * @return array
+     */
     public function getAllArticles(){
-        return $this->databaseManager->selectFromTable(TABLE_ARTICLES);
+        return $this->databaseManager->selectFromTable(TABLE_ARTICLES, "", "datum DESC");
     }
 
+    /**
+     * Metoda vraci všechny články uživatele
+     * @param int $userID
+     * @return array
+     */
     public function getAllUsersArticles(int $userID){
 
-        return $this->databaseManager->selectFromTable(TABLE_ARTICLES, "id_uzivatel=$userID");
+        return $this->databaseManager->selectFromTable(TABLE_ARTICLES, "id_uzivatel=$userID", "datum DESC");
     }
 }
